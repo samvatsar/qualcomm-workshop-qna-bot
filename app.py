f62572b8b48de78ba9ebe0a457b3fd579adc363b
samvatsar <samvatsar@gmail.com>
2024-09-29 21:16:19 +0530

    Streaming responses.

--- app.py
+++ app.py
@@ -1,21 +1,45 @@
 import chainlit as cl
+from openai import AsyncOpenAI
 
-from query_llm import get_response
+client = AsyncOpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+# Instrument the OpenAI client
+cl.instrument_openai()
+
+settings = {
+    "model": "gpt-3.5-turbo",
+    "temperature": 0,
+    # ... more settings
+}
 
 
 @cl.on_chat_start
-async def start():
-
+async def start_chat():
     await cl.Message(
-        author="Assistant", content="Hello! Im an AI assistant. How may I help you?"
+        author="Assistant",
+        content="Hello! I'm an AI assistant. How may I help you?"
     ).send()
+    cl.user_session.set(
+        "message_history",
+        [{"role": "system", "content": "You are a helpful assistant. "
+                                       "Keep your answers within 5 sentences."}],
+    )
 
 
 @cl.on_message
 async def main(message: cl.Message):
+    message_history = cl.user_session.get("message_history")
+    message_history.append({"role": "user", "content": message.content})
 
-    res = get_response(message.content)
+    msg = cl.Message(content="")
+    await msg.send()
 
-    await cl.Message(
-        content=res,
-    ).send()
+    stream = await client.chat.completions.create(
+        messages=message_history, stream=True, **settings
+    )
+
+    async for part in stream:
+        if token := part.choices[0].delta.content or "":
+            await msg.stream_token(token)
+
+    message_history.append({"role": "assistant", "content": msg.content})
+    await msg.update()
--- query_llm.py
+++ query_llm.py
@@ -1,4 +1,5 @@
 import nest_asyncio
+from httpx import stream
 
 nest_asyncio.apply()
 
@@ -6,15 +7,15 @@ from llama_index.llms.lmstudio import LMStudio
 from llama_index.core.base.llms.types import ChatMessage, MessageRole
 
 llm = LMStudio(
-    model_name="lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF",
+    model_name="lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf",
     base_url="http://localhost:1234/v1",
     # temperature=0.7,
     timeout=600
 )
 # response = llm.complete("Hey there, what is 2+2?")
 # print(str(response))
-def get_response(query):
-    print("got request to query llm: {}".format(query))
+async def get_response(query):
+    print("user query: {}".format(query))
     messages = [
         ChatMessage(
             role=MessageRole.SYSTEM,
@@ -29,4 +30,5 @@ def get_response(query):
     response = llm.chat(messages=messages)
     res = str(response).strip("assistant: ")
     print(res)
+    print("*************************************************")
     return res
